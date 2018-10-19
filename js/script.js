1043febf26d78d3f8ab72dae34495a360ede4357
makeoverWeb <femakeoverweb@gmail.com>
2018-10-19 18:13:20 +0300

    v 2.1

--- js/script.js
+++ js/script.js
@@ -10,20 +10,4 @@ $(function() {
     });
   });
 
-anim('.anim-1', 'zoomInUp');
-anim('.anim-2', 'zoomInUp');
-anim('.anim-3', 'zoomInUp');
-anim('.anim-5', 'zoomInUp');
-anim('.anim-6', 'zoomInUp');
-anim('.anim-7', 'fadeInRight');
-
-function anim(name, type) {
-    $(name).css('opacity', 0);
-    console.log("sss");
-    $(name).waypoint(function() {
-        var typ = "animated " + type;
-        $(name).addClass(typ);
-        $(name).css('opacity', 1);
-    }, { offset: '90%' });
-}
 });
